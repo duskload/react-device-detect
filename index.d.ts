db6e56155d1c51ccc94500f2cf2032bcf57b33fc
yosef langer <yosef@doar.systems>
2018-06-11 02:56:33 +0300

    added more typings and fixes

--- index.d.ts
+++ index.d.ts
@@ -13,7 +13,77 @@ declare module 'react-device-detect' {
         render(): JSX.Element;
     }
 
-    export function isBrowser(): boolean
+    export class AndroidView extends React.Component<any, never> {
+        device: any;
 
-    export function isMobile(): boolean
+        render(): JSX.Element;
+    }
+
+    export class IEView extends React.Component<any, never> {
+        device: any;
+
+        render(): JSX.Element;
+    }
+
+
+    export class IOSView extends React.Component<any, never> {
+        device: any;
+
+        render(): JSX.Element;
+    }
+
+    export class TabletView extends React.Component<any, never> {
+        device: any;
+
+        render(): JSX.Element;
+    }
+
+    export class WinPhoneView extends React.Component<any, never> {
+        device: any;
+
+        render(): JSX.Element;
+    }
+
+    export class MobileOnlyView extends React.Component<any, never> {
+        device: any;
+
+        render(): JSX.Element;
+    }
+
+    export class SmartTVView extends React.Component<any, never> {
+        device: any;
+
+        render(): JSX.Element;
+    }
+
+    export class ConsoleView extends React.Component<any, never> {
+        device: any;
+
+        render(): JSX.Element;
+    }
+
+    export class WearableView extends React.Component<any, never> {
+        device: any;
+
+        render(): JSX.Element;
+    }
+
+    export class CustomView extends React.Component<any, never> {
+        device: any;
+
+        render(): JSX.Element;
+    }
+
+    export const isBrowser: boolean;
+
+    export const isMobile: boolean;
+
+    export const isTablet: boolean;
+
+    export const isSmartTV: boolean;
+
+    export const isConsole: boolean;
+
+    export const isWearable: boolean;
+
 }
